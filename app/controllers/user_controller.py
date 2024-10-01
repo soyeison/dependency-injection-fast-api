e3dbf7e556aab4b0e2cac6127c3513063cc75f29
Yeison Villegas <yeisonvillegas07@gmail.com>
2024-10-01 06:09:33 -0500

    Modfificado controlador para incluir codigos de error

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
 from app.services.user_service import UserService
 from app.exceptions.base_exception_class import ServiceException
-from app.exceptions.user_exception import UserAlreadyExistException
 
 class UserController:
     def __init__(self, ):
@@ -13,11 +12,6 @@ class UserController:
         self.router.get("/")(self.get_all)
 
     def get_all(self, limit: int | None = None, user_service: UserService = Depends(UserService)):
-        try:
-            print("limit:", limit)
-            raise UserAlreadyExistException("El usuario no existe")
-            users = user_service.getAll(limit=limit)
-            # encriptar datos
-            return JSONResponse(content={"message": "Todos los usuarios"}, status_code=200)
-        except Exception as e:
-            raise ServiceException(str(e))
+        users = user_service.getAll(limit=limit)
+        # encriptar datos
+        return JSONResponse(content={"message": "Todos los usuarios"}, status_code=200)
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,6 +1,7 @@
 from fastapi import Depends
 from app.database.repositories.user_repository import UserRepository
 from app.database.repositories.account_repository import AccountRepository
+from app.exceptions.user_exception import UserAlreadyExistException
 
 class UserService:
     def __init__(self, user_repository: UserRepository = Depends(UserRepository), account_repository: AccountRepository = Depends(AccountRepository)):
@@ -9,6 +10,8 @@ class UserService:
         self.account_repository = account_repository
 
     def getAll(self, limit: int | None):
+        print("limit:", limit)
+        raise UserAlreadyExistException("El usuario no existe")
         print("Paso con el user service")
         print("Acocunt repository:",self.account_repository.getAll())
         print("User respository:",self.user_repository.getAll(limit=limit))
